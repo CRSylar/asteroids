8ad2a9968b261435816c1ab5e773615c528e9ed7
CRSylar <cristianoromaldetti@gmail.com>
2026-02-11 15:09:15 +0100

    feature: added player ship, asteroids and fields, added collision detection

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
+import sys
 import pygame
+from asteroidfield import AsteroidField
+from asteroids import Asteroid
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
-from logger import log_state
+from logger import log_event, log_state
+from player import Player
 
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     _ = pygame.init()
 
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Player.containers = (updatable, drawable)
+    AsteroidField.containers = updatable
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    _ = AsteroidField()
 
     while True:
         log_state()
@@ -16,7 +33,20 @@ def main():
                 return
 
         _ = screen.fill("black")
+
+        updatable.update(dt)
+
+        for obj in asteroids:
+            if obj.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
